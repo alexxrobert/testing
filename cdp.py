61cdf0b6cc09a6ab26ec0cbe539d0c11e8e36fc4
alexxrobert <38689393+alexxrobert@users.noreply.github.com>
2025-04-06 19:07:38 +0200

    Update cdp.py

--- cdp.py
+++ cdp.py
@@ -1,32 +1,13 @@
-"""Example of using CDP Mode without WebDriver"""
-import asyncio
-from contextlib import suppress
-from seleniumbase import decorators
-from seleniumbase.core import sb_cdp
-from seleniumbase.undetected import cdp_driver
-
+from seleniumbase import SB
 
 START_URL = \
     "https://www.leroymerlin.ro/produse/pereti-despartitori-si-tavane/477"
 
-
-@decorators.print_runtime("CDP Priceline Example")
-def main():
-    url0 = "about:blank"  # Set Locale code from here first
-    url1 = START_URL
-    loop = asyncio.new_event_loop()
-    driver = cdp_driver.cdp_util.start_sync()
-    page = loop.run_until_complete(driver.get(url0))
-    sb = sb_cdp.CDPMethods(loop, page, driver)
-    sb.set_locale("en")  # This test expects English locale
-    sb.open(url1)
+with SB(uc=True, test=True, locale_code="en") as sb:
+    sb.activate_cdp_mode(START_URL)
     sb.sleep(2.5)
-    sb.internalize_links()  # Don't open links in a new tab
-    sb.sleep(3)
-    sb.save_screenshot("now_secure_image.png")
-    print("\nScreenshot saved to: %s\n" % "now_secure_image.png")
-
-
-if __name__ == "__main__":
-    with suppress(Exception):
-        main()
+    sb.uc_gui_click_captcha()
+    sb.sleep(2.5)
+    screenshot_path = "now_secure_image.png"
+    sb.save_screenshot(screenshot_path)
+    print("\nScreenshot saved to: %s\n" % screenshot_path)
