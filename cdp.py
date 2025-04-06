33e00b49da91510a78d7e1755628402dce554bbc
alexxrobert <38689393+alexxrobert@users.noreply.github.com>
2025-04-06 19:21:01 +0200

    Update cdp.py

--- cdp.py
+++ cdp.py
@@ -1,7 +1,8 @@
 from seleniumbase import SB
 
 START_URL = \
-    "https://www.leroymerlin.ro/produse/pereti-despartitori-si-tavane/477/data.json"
+    "https://www.leroymerlin.ro/produse/" + \
+    "pereti-despartitori-si-tavane/477/data.json"
 with SB(uc=True, test=True, locale_code="en") as sb:
     sb.activate_cdp_mode(START_URL)
     sb.sleep(2.5)
