fee0b77da8148f22b1e8fcf2f08bdbc34959b32b
alexxrobert <38689393+alexxrobert@users.noreply.github.com>
2025-04-06 18:48:53 +0200

    Create cdp.py

---
+++ cdp.py
@@ -0,0 +1,37 @@
+"""Example of using CDP Mode without WebDriver"""
+import asyncio
+from contextlib import suppress
+from seleniumbase import decorators
+from seleniumbase.core import sb_cdp
+from seleniumbase.undetected import cdp_driver
+import os
+
+SCREENSHOT_DIR = "screenshots"
+os.makedirs(SCREENSHOT_DIR, exist_ok=True)
+
+START_URL = \
+    "https://www.leroymerlin.ro/produse/pereti-despartitori-si-tavane/477"
+
+
+@decorators.print_runtime("CDP Priceline Example")
+def main():
+    url0 = "about:blank"  # Set Locale code from here first
+    url1 = START_URL
+    loop = asyncio.new_event_loop()
+    driver = cdp_driver.cdp_util.start_sync()
+    page = loop.run_until_complete(driver.get(url0))
+    sb = sb_cdp.CDPMethods(loop, page, driver)
+    sb.set_locale("en")  # This test expects English locale
+    sb.open(url1)
+    sb.sleep(2.5)
+    sb.internalize_links()  # Don't open links in a new tab
+    sb.sleep(3)
+    screenshot_name = f"screenshot_page_0.png"
+    screenshot_path = os.path.join(SCREENSHOT_DIR, screenshot_name)
+    sb.save_screenshot(screenshot_path)
+    print("\nScreenshot saved to: %s\n" % screenshot_path)
+
+
+if __name__ == "__main__":
+    with suppress(Exception):
+        main()
